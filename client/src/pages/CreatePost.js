0a295e059ef16b94f11430c798a00f4e79a3a5de
anabeggiato <ana.beggiato@sou.inteli.edu.br>
2025-01-06 22:48:17 -0300

    docs: add comments to improve code clarity

--- client/src/pages/CreatePost.js
+++ client/src/pages/CreatePost.js
@@ -1,36 +1,36 @@
 import React from 'react';
 import { Formik, Form, Field, ErrorMessage } from 'formik';
-import * as Yup from 'yup';
-import axios from 'axios';
+import * as Yup from 'yup'; //importing a library to simplificate form validations
+import axios from 'axios'; //library to create an require into our api
 
 function CreatePost() {
 
-    const initialValues = {
+    const initialValues = { //creating an empty objetc with the initial values, to send it into the formik parameter
         title: '',
         postText: '',
         username: ''
     };
 
-    const validationSchema = Yup.object().shape({
-        title: Yup.string().required(),
-        postText: Yup.string().required(),
-        username: Yup.string().min(3).max(15).required(),
+    const validationSchema = Yup.object().shape({ //creating a validation schema, which will be used as a parameter to formik
+        title: Yup.string().required("You must define a title to your post!"), // define that the title must be a string and must not be null.
+        postText: Yup.string().required("You must write a content to your post!"), // define that the postText must be a string and must not be null.
+        username: Yup.string().min(3).max(15).required("Your post must contain your username!"), // define that the username must be string, be between 3 and 15 characters long and must not be null.
     });
 
-    const onSubmit = (data) => {
-        axios.post('http://localhost:3001/posts', data).then((response) => {
-            console.log("IT WORKED");
+    const onSubmit = (data) => { // define a function to run when the submit button is clicked
+        axios.post('http://localhost:3001/posts', data).then((response) => { //go to the post route and post the data in the db
+            console.log("IT WORKED"); //log that the post is okay
         })
     }
 
     return (
         <div className='createPostPage'>
-            <Formik initialValues={initialValues} onSubmit={onSubmit} validationSchema={validationSchema}>
-                <Form className='formContainer'>
+            <Formik initialValues={initialValues} onSubmit={onSubmit} validationSchema={validationSchema}> {/* intantiating Formik with the objects which were previously created*/}
+                <Form className='formContainer'> {/*instantiating a form*/}
                     
                     <label>Title:</label>
-                    <ErrorMessage name='title' component='span' />
-                    <Field
+                    <ErrorMessage name='title' component='span' /> {/* define where the error message must appear, and which type it must assume (here, it's defined as a span) */}
+                    <Field //define a field where the user will fill with the post title
                         autocomplete='off'
                         id='inputCreatePost'
                         name='title'
@@ -39,7 +39,7 @@ function CreatePost() {
 
                     <label>Post:</label>
                     <ErrorMessage name='postText' component='span' />
-                    <Field
+                    <Field //define a field where the user will fill with the post content
                         autocomplete='off'
                         id='inputCreatePost'
                         name='postText'
@@ -48,14 +48,14 @@ function CreatePost() {
 
                     <label>Username:</label>
                     <ErrorMessage name='username' component='span' />
-                    <Field
+                    <Field //define a field where the user will fill with his username
                         autocomplete='off'
                         id='inputCreatePost'
                         name='username'
                         placeholder='write your username here'
                     />
 
-                    <button type='submit' > Create Post </button>
+                    <button type='submit' >  Create Post </button> {/*button to be clicked when the post is complete, to send it to the db and to the homepage */}
                 </Form>
             </Formik>
         </div>
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -5,21 +5,21 @@ import { useEffect, useState } from 'react'
 
 function Home() {
 
-    const [listOfPosts, setListOfPosts] = useState([]);
+    const [listOfPosts, setListOfPosts] = useState([]); //defining a variable which will receive the list of posts when it comes from the db
 
-    useEffect(() => { //login to run immediately when the page renders
+    useEffect(() => { //function to run immediately when the page renders
         axios.get('http://localhost:3001/posts').then((response) => {
-            setListOfPosts(response.data)
+            setListOfPosts(response.data) //storing the response data (the list os posts) in the variable previously created
         })
-    }, []) //list of states which will trigger like the function to run again
+    }, [])
 
     return (
         <div>
-            {listOfPosts.map((value, key) => {
-                return <div className='post'>
-                    <div className='title'> {value.title} </div>
-                    <div className='body'> {value.postText} </div>
-                    <div className='footer'> {value.username} </div>
+            {listOfPosts.map((value, key) => { //mapping all the db posts
+                return <div className='post'> {/*creating a div for each db post */}
+                    <div className='title'> {value.title} </div> {/*a specific div for each post title*/}
+                    <div className='body'> {value.postText} </div> {/*a specific div for each post content*/}
+                    <div className='footer'> {value.username} </div> {/*a specific div for each post username*/}
                 </div>;
             })}
         </div>
